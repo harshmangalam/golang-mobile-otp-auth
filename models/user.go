d2d3afc7b97af3276dfda05d3b6472682c4afdab
harshmangalam <harshdev8218@gmail.com>
2023-08-04 02:31:56 +0530

    added User model

---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID    primitive.ObjectID `json:"id" bson:"_id"`
+	Name  string             `json:"name"`
+	Phone string             `json:"phone"`
+}
