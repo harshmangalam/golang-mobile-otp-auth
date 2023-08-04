45202c3e90c547a698c6fe4c4e9ca91461d26f1d
harshmangalam <harshdev8218@gmail.com>
2023-08-05 00:44:13 +0530

    get user by id

--- utils/user.go
+++ utils/user.go
@@ -69,6 +69,36 @@ func UpdateUser(userID primitive.ObjectID, updatedFields map[string]any) error {
 
 }
 
+func FindUserById(userId string) (*models.User, error) {
+	// Create a context and a collection instance
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.TODO()
+	collection := database.Mg.Db.Collection(database.Users)
+
+	// Create a filter to find the user by phone number
+	filter := bson.M{"_id": id}
+
+	// Create a variable to store the result
+	var result models.User
+
+	// Find the user with the given phone number
+	err = collection.FindOne(ctx, filter).Decode(&result)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// If the error is ErrNoDocuments, it means no user was found
+			return nil, nil
+		}
+		// Handle other potential errors
+		return nil, err
+	}
+
+	return &result, nil
+}
+
 func GenerateRandomNumber() string {
 	// Generate a random number between 1000 and 9999 (inclusive)
 	num := rand.Intn(9000) + 1000
