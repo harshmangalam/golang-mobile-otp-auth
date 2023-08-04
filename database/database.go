247807e116c7a0a408903e62173771e4c39f74bf
harshmangalam <harshdev8218@gmail.com>
2023-08-04 20:03:58 +0530

    register user

--- database/database.go
+++ database/database.go
@@ -2,6 +2,8 @@ package database
 
 import "go.mongodb.org/mongo-driver/mongo"
 
+const Users = "users"
+
 type MongoInstance struct {
 	Client *mongo.Client
 	Db     *mongo.Database
--- handlers/auth.go
+++ handlers/auth.go
@@ -1,11 +1,59 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"auth/schema"
+	"auth/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Register(c *fiber.Ctx) error {
+	// incomming body data
+	body := new(schema.RegisterBody)
+	if err := c.BodyParser(body); err != nil {
+		return c.JSON(ResponseHTTP{
+			Success: false,
+			Data:    nil,
+			Message: err.Error(),
+		})
+	}
+
+	// validate duplicate mobile number
+
+	user, err := utils.FindUserByPhone(body.Phone)
+
+	if err != nil {
+		return c.JSON(ResponseHTTP{
+			Success: false,
+			Data:    nil,
+			Message: err.Error(),
+		})
+	}
+
+	if user != nil {
+		return c.JSON(ResponseHTTP{
+			Success: false,
+			Data:    nil,
+			Message: "Phone number already in use",
+		})
+	}
+
+	// create new user
+
+	id, err := utils.InsertUser(body)
+	if err != nil {
+		return c.JSON(ResponseHTTP{
+			Success: false,
+			Data:    nil,
+			Message: err.Error(),
+		})
+	}
+
 	return c.JSON(ResponseHTTP{
 		Success: true,
-		Data:    nil,
+		Data: fiber.Map{
+			"id": id,
+		},
 		Message: "Account registered successfully",
 	})
 }
